6c24a8a329f419cd7cc930fba5de6382fa6f9df2
Kumar McMilllan <kumar.mcmillan@gmail.com>
2007-12-02 22:20:37 +0000

    turned on doctest for nose and hacked webunit.config so it can be imported when nose looks for doctests

--- wsgi_intercept/webunit/config.py
+++ wsgi_intercept/webunit/config.py
@@ -1,36 +1,33 @@
-'''
+"""
 This file allows you to set up configuration variables to identify the
 machine and port to test.
 
 It needs some work, but in a nutshell, put a config.cfg in your "test"
-directory with the following contents:
+directory with the following contents::
 
----- snip
-
-[DEFAULT]
-machine = www.dev.ekorp.com
-port = 80
-
-[dev-ekit]
-# uses DEFAULT
-
-[dev-lp]
-machine = www.lonelyplanet.dev.ekorp.com
-port = 80
-
----- snip
+    [DEFAULT]
+    machine = www.dev.ekorp.com
+    port = 80
+    
+    [dev-ekit]
+    # uses DEFAULT
+    
+    [dev-lp]
+    machine = www.lonelyplanet.dev.ekorp.com
+    port = 80
 
 Then set the environment var "TEST_CONFIG" to the config to use.
 
-'''
-import ConfigParser, os
+"""
+import os
+if os.path.exists('test/config.cfg'):
+    import ConfigParser
+    cfg = ConfigParser.ConfigParser()
+    cfg.read('test/config.cfg')
 
-cfg = ConfigParser.ConfigParser()
-cfg.read('test/config.cfg')
+    # figure the active config
+    active = os.environ.get('TEST_CONFIG', 'DEFAULT')
 
-# figure the active config
-active = os.environ.get('TEST_CONFIG', 'DEFAULT')
-
-# fetch the actual config info
-machine = cfg.get(active, 'machine')
-port = cfg.getint(active, 'port')
+    # fetch the actual config info
+    machine = cfg.get(active, 'machine')
+    port = cfg.getint(active, 'port')
