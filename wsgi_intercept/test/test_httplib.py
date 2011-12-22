cc6448053933e8b96a2e50c2f4012419656fcc97
Michael Mulich <michael.mulich@gmail.com>
2011-12-22 00:47:47 -0500

    httplib tests rewritten using unittest instead of nose

--- wsgi_intercept/test/test_httplib.py
+++ wsgi_intercept/test/test_httplib.py
@@ -1,48 +1,44 @@
-#! /usr/bin/env python2.4
-from nose.tools import with_setup, raises, eq_
-from wsgi_intercept import httplib_intercept
-from socket import gaierror
-import wsgi_intercept
-from wsgi_intercept import test_wsgi_app
 import httplib
-
-_saved_debuglevel = None
-
-
-def http_install():
-    _saved_debuglevel, wsgi_intercept.debuglevel = wsgi_intercept.debuglevel, 1
-    httplib_intercept.install()
-    wsgi_intercept.add_wsgi_intercept('some_hopefully_nonexistant_domain', 80, test_wsgi_app.create_fn)
-
-def http_uninstall():
-    wsgi_intercept.debuglevel = _saved_debuglevel
-    wsgi_intercept.remove_wsgi_intercept('some_hopefully_nonexistant_domain', 80)
-    httplib_intercept.uninstall()
-
-@with_setup(http_install, http_uninstall)
-def test_http_success():
-    http = httplib.HTTPConnection('some_hopefully_nonexistant_domain')
-    http.request('GET', '/')
-    content = http.getresponse().read()
-    eq_(content, 'WSGI intercept successful!\n')
-    assert test_wsgi_app.success()
+import wsgi_intercept
+from wsgi_intercept import testing
+from wsgi_intercept.testing import unittest
 
 
+class HttpTestCase(unittest.TestCase):
+    port = 80
 
-def https_install():
-    _saved_debuglevel, wsgi_intercept.debuglevel = wsgi_intercept.debuglevel, 1
-    httplib_intercept.install()
-    wsgi_intercept.add_wsgi_intercept('some_hopefully_nonexistant_domain', 443, test_wsgi_app.create_fn)
+    @property
+    def connection_cls(self):
+        return httplib.HTTPConnection
 
-def https_uninstall():
-    wsgi_intercept.debuglevel = _saved_debuglevel
-    wsgi_intercept.remove_wsgi_intercept('some_hopefully_nonexistant_domain', 443)
-    httplib_intercept.uninstall()
-    
-@with_setup(https_install, https_uninstall)
-def test_https_success():
-    http = httplib.HTTPSConnection('some_hopefully_nonexistant_domain')
-    http.request('GET', '/')
-    content = http.getresponse().read()
-    eq_(content, 'WSGI intercept successful!\n')
-    assert test_wsgi_app.success()
+    def setUp(self):
+        _saved_debuglevel = wsgi_intercept.debuglevel
+        wsgi_intercept.debuglevel = 1
+        self.addCleanup(setattr,
+                        wsgi_intercept, 'debuglevel', _saved_debuglevel)
+        # Install the intercept
+        from wsgi_intercept import httplib_intercept
+        httplib_intercept.install()
+        # Add the intercept for a nonexistant domain
+        self.domain = 'some_hopefully_nonexistant_domain'
+        wsgi_intercept.add_wsgi_intercept(self.domain, self.port,
+                                          testing.create_fn)
+        self.addCleanup(wsgi_intercept.remove_wsgi_intercept,
+                        self.domain, self.port)
+        # Cleanup with an intercept uninstall
+        self.addCleanup(httplib_intercept.uninstall)
+
+    def test_success(self):
+        http = self.connection_cls(self.domain)
+        http.request('GET', '/')
+        content = http.getresponse().read()
+        self.assertEqual(content, 'WSGI intercept successful!\n')
+        self.assertTrue(testing.success())
+
+
+class HttpsTestCase(HttpTestCase):
+    port = 443
+
+    @property
+    def connection_cls(self):
+        return httplib.HTTPSConnection
