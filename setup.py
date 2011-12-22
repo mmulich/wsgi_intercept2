108259245b7e7f905c178d7d8f70278eef1fcf7b
Michael Mulich <michael.mulich@gmail.com>
2011-12-21 23:45:21 -0500

    Spacing style

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ readme = os.path.join(here, "README.rst")
 long_description = open(readme).read()
 
 setup(
-    name = 'wsgi_intercept',
-    version = version,
-    author = 'Titus Brown, Kumar McMillan',
-    author_email = 'kumar.mcmillan@gmail.com',
-    description = description,
+    name='wsgi_intercept',
+    version=version,
+    author='Titus Brown, Kumar McMillan',
+    author_email='kumar.mcmillan@gmail.com',
+    description=description,
     url="http://code.google.com/p/wsgi-intercept/",
-    long_description = long_description,
-    license = 'MIT License',
-    packages = find_packages(),
-    test_suite = "nose.collector",
+    long_description=long_description,
+    license='MIT License',
+    packages=find_packages(),
+    test_suite="nose.collector",
     tests_require=[
         'nose',
         'Paste',
