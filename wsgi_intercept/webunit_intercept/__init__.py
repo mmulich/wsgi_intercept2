cd2c339aeab5da8c5e7cb13e008eb8efb7a4dbf9
Michael Mulich <michael.mulich@gmail.com>
2011-12-23 01:32:32 -0500

    webunit removal, closes #26

--- wsgi_intercept/webunit_intercept/__init__.py
+++
@@ -1,8 +0,0 @@
-
-"""work intercepted HTTP connectsion in a webunit test case.
-
-(see wsgi_intercept/__init__.py for examples)
-            
-"""
-from webunittest import WebTestCase
-__version__ = '1.3.8'
