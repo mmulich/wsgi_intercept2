2ce08428df973174b2a8517b00905c050a90d5eb
Michael Mulich <michael.mulich@gmail.com>
2011-12-23 14:24:52 -0500

    urllib2 tests upgraded to use unittest.

--- wsgi_intercept/test/test_wsgi_urllib2.py
+++ wsgi_intercept/test/test_wsgi_urllib2.py
@@ -1,45 +1,27 @@
-#! /usr/bin/env python
-import sys, os.path
-from nose.tools import with_setup
 import urllib2
-from wsgi_intercept import urllib2_intercept
-import wsgi_intercept
-from wsgi_intercept import test_wsgi_app
+from wsgi_intercept import testing
+from wsgi_intercept.test import base
 
-_saved_debuglevel = None
 
-def add_http_intercept():
-    _saved_debuglevel, wsgi_intercept.debuglevel = wsgi_intercept.debuglevel, 1
-    wsgi_intercept.add_wsgi_intercept('some_hopefully_nonexistant_domain', 80, test_wsgi_app.create_fn)
-    
-def add_https_intercept():
-    _saved_debuglevel, wsgi_intercept.debuglevel = wsgi_intercept.debuglevel, 1
-    wsgi_intercept.add_wsgi_intercept('some_hopefully_nonexistant_domain', 443, test_wsgi_app.create_fn)
+class Urllib2HttpTestCase(base.BaseTestCase):
 
-def remove_intercept():
-    wsgi_intercept.debuglevel = _saved_debuglevel
-    wsgi_intercept.remove_wsgi_intercept()
+    def setUp(self):
+        from wsgi_intercept.urllib2_intercept import install_opener
+        from wsgi_intercept.urllib2_intercept import uninstall_opener
+        install_opener()
+        self.addCleanup(uninstall_opener)
+        super(Urllib2HttpTestCase, self).setUp()
 
-@with_setup(add_http_intercept, remove_intercept)
-def test_http():
-    urllib2_intercept.install_opener()
-    urllib2.urlopen('http://some_hopefully_nonexistant_domain:80/')
-    assert test_wsgi_app.success()
-    
-@with_setup(add_http_intercept, remove_intercept)
-def test_http_default_port():
-    urllib2_intercept.install_opener()
-    urllib2.urlopen('http://some_hopefully_nonexistant_domain/')
-    assert test_wsgi_app.success()
-    
-@with_setup(add_https_intercept, remove_intercept)
-def test_https():
-    urllib2_intercept.install_opener()
-    urllib2.urlopen('https://some_hopefully_nonexistant_domain:443/')
-    assert test_wsgi_app.success()
-    
-@with_setup(add_https_intercept, remove_intercept)
-def test_https_default_port():
-    urllib2_intercept.install_opener()
-    urllib2.urlopen('https://some_hopefully_nonexistant_domain/')
-    assert test_wsgi_app.success()
+    def test_success(self):
+        urllib2.urlopen(self.url)
+        self.assertTrue(testing.success())
+
+    def test_default_port(self):
+        url = self.url
+        url.replace(':%s' % self.port, '')
+        urllib2.urlopen(url)
+        self.assertTrue(testing.success())
+
+
+class Urllib2HttpsTestCase(Urllib2HttpTestCase):
+    port = 443
