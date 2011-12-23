513ec8709ec787bf55563f271299a8ce4941a057
Michael Mulich <michael.mulich@gmail.com>
2011-12-23 14:14:22 -0500

    Skip the test when DNS resolves any name. If the environment variable (FUNKY_DNS_RESOLUTION) is set the test will skip.

--- wsgi_intercept/test/test_httplib2.py
+++ wsgi_intercept/test/test_httplib2.py
@@ -1,7 +1,9 @@
+import os
 from socket import gaierror
 from wsgi_intercept import testing
 from wsgi_intercept.testing import unittest
 from wsgi_intercept.test import base
+
 try:
     import httplib2
     has_httplib2 = True
@@ -9,14 +11,23 @@ except ImportError:
     has_httplib2 = False
 _skip_message = "httplib2 is not installed"
 
+_value = os.environ.get('FUNKY_DNS_RESOLUTION', None)
+has_funky_dns_resolution = _value is None and False or True
+_isp_skip_message = "DNS resolves any given name. Therefore, the results of this test are invalid."
+
 
 @unittest.skipUnless(has_httplib2, _skip_message)
 class Httplib2HttpTestCase(base.BaseHttplib2TestCase):
     port = 80
 
+    @unittest.skipIf(has_funky_dns_resolution, _isp_skip_message)
     def test_bogus_domain(self):
         from wsgi_intercept.httplib2_intercept import HTTP_WSGIInterceptorWithTimeout
         with self.assertRaises(gaierror):
+            # NOTE This test will fail if your ISP or DNS provider resolves
+            #   all names. For example, Verizon resolves all names and in most
+            #   cases tries to redirect you to a web search page.
+            #   See also the unittest skip message.
             HTTP_WSGIInterceptorWithTimeout("_nonexistant_domain_").connect()
 
 
